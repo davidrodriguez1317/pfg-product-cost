cac6de5443809262b8215d623da7a5f5d0939d58
davidrodriguez1317 <davidrodriguez1317@gmail.com>
2023-02-24 08:34:09 +0100

    Springboot project created

---
+++ src/main/java/com/dro/pfg/pfgproductcost/PfgProductCostApplication.java
@@ -0,0 +1,13 @@
+package com.dro.pfg.pfgproductcost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfgProductCostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfgProductCostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dro/pfg/pfgproductcost/PfgProductCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dro.pfg.pfgproductcost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfgProductCostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
